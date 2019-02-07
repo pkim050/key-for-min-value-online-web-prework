02a304ce5e4f55ec16490f5bad8b946a339b0d34
Patrick Kim <pkim050@gmail.com>
2019-02-07 18:39:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,6 @@
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
   name_hash = name_hash.sort_by {|k, v| v}
-  min_value = name_hash.sort_by {|k, v| -v}.first
+  min_value = name_hash.sort_by {|k, v| -v}.[0,1]
   min_value
 end
