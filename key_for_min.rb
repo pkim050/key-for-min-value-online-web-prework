1fe802971762d8755c4505a318cf1ec16f96f1f8
Patrick Kim <pkim050@gmail.com>
2019-02-07 18:55:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,13 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
-  
+  counter = 0
+  name_hash.each do |k, v|
+    if counter == 0
+      min_value = v
+    else
+      min_value = v if v < min_value
+    end
+  end
   min_value
 end
